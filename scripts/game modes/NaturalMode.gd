64a969972c255c37291860019f9666f18a4496ff
Nickellio <lerbasso@gmail.com>
2024-08-13 22:37:42 +0200

    Added 4 Zebras in Natural mode

--- scripts/game modes/NaturalMode.gd
+++ scripts/game modes/NaturalMode.gd
@@ -1,1 +1,19 @@
 extends GameMode
+
+var auto_pieces = []
+
+func _ready():
+	super()
+	var positions = ["1-2", "2-1", 
+	str(Global.board_width) + "-" + str(Global.board_height - 1),
+	str(Global.board_width - 1) + "-"  + str(Global.board_height)
+	]
+	for pos in positions:
+		auto_pieces.append(Zebra.new("neutral", pos))
+		$ChessboardRect/Chessboard.set_piece(auto_pieces[-1])
+
+func end_turn():
+	for piece in auto_pieces:
+		var pos = piece.auto_move()
+		$ChessboardRect/Chessboard.traverse(piece, pos)
+	super()
