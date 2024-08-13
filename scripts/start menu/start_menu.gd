4986749986e16d1a352ff68581876c4b307b71ab
Nickellio <lerbasso@gmail.com>
2024-08-13 17:02:03 +0200

    Added list with game modes

--- scripts/start menu/start_menu.gd
+++ scripts/start menu/start_menu.gd
@@ -1,10 +1,16 @@
 extends Control
 
+func _ready():
+	$ItemList.select(0)
+
 func _on_submit_pressed():
 	var height = $Height.get_value()
 	var width = $Width.get_value()
+	var game_mode = $ItemList.get_selected_items()
+	var next_scene = "res://scenes/" + ("classic_mode.tscn" if game_mode[0] == 0 else "natural_mode.tscn")
+	
 	if (width - height) % 2 == 0:
 		Global.board_width = width
 		Global.board_height = height
-		get_tree().change_scene_to_file("res://scenes/gui.tscn")
+		get_tree().change_scene_to_file(next_scene)
 		
