dc30aed516db9cded8c15316174f0f4d143f965b
Nickellio <lerbasso@gmail.com>
2024-08-14 17:30:45 +0200

    Neutral teams
    Added neutral teams, modified find_attackable to not search through neutrals, added new teams constant

--- scripts/pieces/BasePiece.gd
+++ scripts/pieces/BasePiece.gd
@@ -86,11 +86,40 @@ func find_reachable() -> Array:
 ## Finds all squares, distance from coords to which is not greater then range
 ## Returns only squares, that are attackable. 
 func find_attackable():
-	var neighbors = Board.get_neighbors(coords, range)
+	var xy = Board.coords_to_int(coords)
+	
+	var visited = []
+	for i in Global.board_height + 2:
+		visited.append([])
+		for j in Global.board_width + 2:
+			visited[i].append(false)
+	visited[xy[0]][xy[1]] = true
+	
+	var square_queue = [[coords, 0]] ## Queue saves last visited square and distance to it
 	var attackable = []
-	for neighbor in neighbors:
-		if Board.is_enemy(color[0], neighbor) and not Board.is_enemy_king(color[0], neighbor):
-			attackable.append(neighbor)
+	
+	while not square_queue.is_empty():
+		var square = square_queue.pop_front()
+		
+		## When DFS reaches the point where distance equals to speed, return result
+		if square[1] == range: 
+			return attackable
+		
+		square[1] += 1 ## Distance increase
+		var neighbors = Board.get_neighbors(square[0], 1)
+		
+		for elem in neighbors: ## Going through neighbors
+			xy = Board.coords_to_int(elem)
+			
+			## Marking all of the visited squares (piece travels through black squares)
+			if not visited[xy[0]][xy[1]]:
+				visited[xy[0]][xy[1]] = true
+				
+				## If square is an enemy AND not an enemy king, its attackable
+				if Board.is_enemy(color[0], elem) and not Board.is_enemy_king(color[0], elem):
+					attackable.append(elem)
+				if not Board.is_neutral(color[0], elem):
+					square_queue.append([elem, square[1]])
 	return attackable
 
 ## Changes coordinates to a new value
