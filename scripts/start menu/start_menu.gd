27df956c2963aa66909d884cae669b198254c97e
George-Mikulich <g.mikulich@softteco.com>
2024-08-17 16:44:23 +0300

    num of Zebras from size

--- scripts/start menu/start_menu.gd
+++ scripts/start menu/start_menu.gd
@@ -10,7 +10,7 @@ func _on_submit_pressed():
 	var next_scene = "res://scenes/" + ("classic_mode.tscn" if game_mode[0] == 0 else "natural_mode.tscn")
 	
 	if (width - height) % 2 == 0:
-		Global.board_width = width
-		Global.board_height = height
+		Global.board_width = width if width < 24 else 24
+		Global.board_height = height if width < 18 else 18
 		get_tree().change_scene_to_file(next_scene)
 		
