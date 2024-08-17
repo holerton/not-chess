405cbdd60aeec4045452a89c0169a1e5b415cc39
Guga Mikulich <aragornguga@gmail.com>
2024-08-17 16:44:23 +0300

    num of Zebras from size

--- scripts/game modes/NaturalMode.gd
+++ scripts/game modes/NaturalMode.gd
@@ -4,12 +4,25 @@ var auto_pieces = []
 
 func _ready():
 	super()
-	var positions = [Board.int_to_coords([1, 2]), Board.int_to_coords([2, 1]),
-	Board.int_to_coords([Global.board_width, Global.board_height - 1]),
-	Board.int_to_coords([Global.board_width - 1, Global.board_height])
+	var size = Global.board_height * Global.board_width 
+	var num_of_zebras = size / 30 # 64 / 30 = 2
+	var positions = [Board.int_to_coords([1, 2]),
+	Board.int_to_coords([Global.board_width, Global.board_height - 1]), 
+	Board.int_to_coords([Global.board_width - 3, Global.board_height]) ,
+	Board.int_to_coords([4, 1]),
+	Board.int_to_coords([Global.board_width - 1, Global.board_height]),
+	Board.int_to_coords([2, 1]),
+	Board.int_to_coords([1, 4]),
+	Board.int_to_coords([Global.board_width, Global.board_height - 3]),
+	Board.int_to_coords([3, 2]),
+	Board.int_to_coords([Global.board_width - 2, Global.board_height - 1]),
+	Board.int_to_coords([Global.board_width - 1, Global.board_height - 2]),
+	Board.int_to_coords([2, 3]),
+	Board.int_to_coords([6, 1]),
+	Board.int_to_coords([Global.board_width - 5, Global.board_height])
 	]
-	for pos in positions:
-		auto_pieces.append(Zebra.new("neutral", pos))
+	for i in range(num_of_zebras) :
+		auto_pieces.append(Zebra.new("neutral", positions[i]))
 		$ChessboardRect/Chessboard.set_piece(auto_pieces[-1])
 
 func end_turn():
