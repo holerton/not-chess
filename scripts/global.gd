f5b2bbfdfcdaa3de0c8ce04b5655515c373ef8a0
Nickellio <lerbasso@gmail.com>
2024-08-28 14:37:28 +0200

    Added weather textures

--- scripts/global.gd
+++ scripts/global.gd
@@ -18,10 +18,12 @@ var limits = {'Pawn': 8, "Night": 2, "Bishop": 2, "Rook": 2, "King": 1}
 
 var TERRAIN_TEXTURES: Dictionary = {}
 
-const TERRAIN_COLORS: Dictionary = {"Plain": Color.LAWN_GREEN,
-"Forest": Color.FOREST_GREEN, "Water": Color.DODGER_BLUE,
-"Desert": Color.CORNSILK, "Marsh": Color.DARK_OLIVE_GREEN,
-"Mountain": Color.DARK_GRAY, "Black": Color.DARK_SLATE_GRAY, "White": Color.ALICE_BLUE}
+const TERRAIN_COLORS: Dictionary = {
+"Plain": Color.LAWN_GREEN, "Forest": Color.FOREST_GREEN, 
+"Water": Color.DODGER_BLUE, "Desert": Color.CORNSILK, 
+"Marsh": Color.DARK_OLIVE_GREEN, "Mountain": Color.DARK_GRAY, 
+"Black": Color.DARK_SLATE_GRAY, "White": Color.ALICE_BLUE 
+}
 
 func _ready():
 	set_textures()
@@ -30,16 +32,23 @@ func set_textures():
 	var terrains = ["Plain", "Forest", "Water", "Desert", "Marsh", "Mountain"]
 	
 	for terrain in terrains:
-		TERRAIN_TEXTURES[terrain] = load("res://images/terrains/%s.png" % terrain)
+		TERRAIN_TEXTURES[terrain] = {}
+		TERRAIN_TEXTURES[terrain]["None"] = load("res://images/terrains/%s.png" 
+		% terrain)
 	
-	TERRAIN_TEXTURES["Black"] = create_gradient(Color.DARK_SLATE_GRAY)
-	TERRAIN_TEXTURES["White"] = create_gradient(Color.ALICE_BLUE)
+	TERRAIN_TEXTURES["Mountain"]["Snow"] = create_gradient(Color.DARK_GRAY)
+	TERRAIN_TEXTURES["Water"]["Snow"] = create_gradient(Color.LIGHT_BLUE)
+	
+	TERRAIN_TEXTURES["Black"] = {} 
+	TERRAIN_TEXTURES["Black"]["None"] = create_gradient(Color.DARK_SLATE_GRAY)
+	TERRAIN_TEXTURES["White"] = {} 
+	TERRAIN_TEXTURES["White"]["None"] = create_gradient(Color.ALICE_BLUE)
 
-func create_gradient(color: Color):
-	var grad = GradientTexture2D.new()
-	grad.width = tile_size
-	grad.height = tile_size
-	grad.gradient = Gradient.new()
-	grad.gradient.set_color(0, color)
-	grad.gradient.remove_point(1)
-	return grad
+func create_gradient(color: Color) -> GradientTexture2D:
+	var rect_texture = GradientTexture2D.new()
+	rect_texture.width = tile_size
+	rect_texture.height = tile_size
+	rect_texture.gradient = Gradient.new()
+	rect_texture.gradient.set_color(0, color)
+	rect_texture.gradient.remove_point(1)
+	return rect_texture
