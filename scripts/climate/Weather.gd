c683104e6d40e8437f6568f0f3d75bb197808127
Nickellio <lerbasso@gmail.com>
2024-08-31 22:46:43 +0200

    Changed weather to work on all terrains

--- scripts/climate/Weather.gd
+++ scripts/climate/Weather.gd
@@ -3,10 +3,9 @@ class_name Weather
 
 var terrain_types: Dictionary = {}
 var affected_squares: Array = []
-var appearance_probabilities: Array = []
-var disappearance_probabilities: Array = []
+var appearance_probabilities: Dictionary = {}
+var disappearance_probabilities: Dictionary = {}
 var movable: bool = false
-var initial_probability: int = 0
 
 func _init(terrain_types: Dictionary):
 	self.terrain_types = terrain_types
@@ -14,9 +13,9 @@ func _init(terrain_types: Dictionary):
 func initial_weather():
 	var changed_squares = []
 	for terrain in terrain_types:
+		var prob = appearance_probabilities[terrain][-1]
 		for square in terrain_types[terrain]:
-			var square_status = appear_or_disappear(square, 
-			initial_probability, 0)
+			var square_status = appear_or_disappear(square, prob, 0)
 			if square_status:
 				changed_squares.append(square)
 	return changed_squares
@@ -38,9 +37,10 @@ func appear_or_disappear(square: String, app_prob: int, dis_prob: int):
 func change_weather(month: int):
 	var changed_squares = []
 	for terrain in terrain_types:
+		var app_prob = appearance_probabilities[terrain][month]
+		var dis_prob = disappearance_probabilities[terrain][month]
 		for square in terrain_types[terrain]:
-			var square_status = appear_or_disappear(square,
-			appearance_probabilities[month], disappearance_probabilities[month])
+			var square_status = appear_or_disappear(square, app_prob, dis_prob)
 			if square_status != 0:
 				changed_squares.append(square)
 	return changed_squares
