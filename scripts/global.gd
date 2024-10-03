e507daf6ac3fa2c1e0bfad6121317e5899b58dd2
Nickellio <lerbasso@gmail.com>
2024-10-03 16:58:48 +0200

    Moved piece textures in global

--- scripts/global.gd
+++ scripts/global.gd
@@ -17,6 +17,8 @@ var teams = {-1: "b", 0: "n", 1: "w"}
 var limits = {'Pawn': 8, "Night": 2, "Bishop": 2, "Rook": 2, "King": 1}
 
 var TERRAIN_TEXTURES: Dictionary = {}
+var PIECE_TEXTURES: Dictionary = {}
+var MY_INF: int = 10000
 
 const TERRAIN_COLORS: Dictionary = {
 "Plain": Color.LAWN_GREEN, "Forest": Color.FOREST_GREEN, 
@@ -40,6 +42,16 @@ func set_textures():
 	TERRAIN_TEXTURES["Black"]["None"] = create_gradient(Color.DARK_SLATE_GRAY)
 	TERRAIN_TEXTURES["White"] = {} 
 	TERRAIN_TEXTURES["White"]["None"] = create_gradient(Color.ALICE_BLUE)
+	var colors = ["w", "b"]
+	var quantities = {"K": 1, "Q": 1, "R": 2, "B": 1, "N": 1, "P": 1}
+	for color in colors:
+		for shortname in quantities:
+			var key = color + shortname
+			PIECE_TEXTURES[key] = []
+			for i in range(quantities[shortname]):
+				var image_path = "res://images/" + key + str(i) + ".svg"
+				PIECE_TEXTURES[key].append(load(image_path))
+	PIECE_TEXTURES["nZ"] = [load("res://images/nZ0.svg")]
 
 func create_gradient(color: Color) -> GradientTexture2D:
 	var rect_texture = GradientTexture2D.new()
